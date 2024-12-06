737a6560d516722856d22b08fdecef3741c69ab1
Adel Sehic <adelsehic95@gmail.com>
2024-12-06 10:57:37 +0100

    - Initialize project as package, add helpers

---
+++ helpers/coords.go
@@ -0,0 +1,51 @@
+package helpers
+
+type Coord struct {
+	y int
+	x int
+}
+
+func (c *Coord) right() *Coord {
+	return &Coord{c.y, c.x + 1}
+}
+
+func (c *Coord) left() *Coord {
+	return &Coord{c.y, c.x - 1}
+}
+
+func (c *Coord) up() *Coord {
+	return &Coord{c.y - 1, c.x}
+}
+
+func (c *Coord) down() *Coord {
+	return &Coord{c.y + 1, c.x}
+}
+
+// Get top-left edge (0, 0)
+func (c *Coord) topLeft() *Coord {
+	return c.up().left()
+}
+
+// Get top-right edge
+func (c *Coord) topRight() *Coord {
+	return c.up().right()
+}
+
+// Get bottom-left edge
+func (c *Coord) bottomLeft() *Coord {
+	return c.down().left()
+}
+
+// Get bottom-right edge
+func (c *Coord) bottomRight() *Coord {
+	return c.down().right()
+}
+
+// Get all neighbors
+func (c *Coord) neighbors() []*Coord {
+	return []*Coord{
+		c.topLeft(), c.up(), c.topRight(),
+		c.left(), c.right(),
+		c.bottomLeft(), c.down(), c.bottomRight(),
+	}
+}

fd1aa1eb19b375ad41739da2ea4906ad70a6b1d4
Adel Sehic <adelsehic95@gmail.com>
2024-12-10 01:25:21 +0100

    - Day9 benchmark added

--- day9/main.go
+++ day9/main.go
@@ -3,20 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	in := MakeInput(os.Args[1])
 	in.ExpandLines()
 	part1 := in.Lines[0].Copy()
 	part2 := in.Lines[0].Copy()
 
 	part1.DefragmentBreak()
-	fmt.Println("Part1: ", part1.Checksum())
+	fmt.Println("Part1: ", part1.Checksum(), time.Since(start))
 
 	// fmt.Println(part2.Decode())
 	part2.DefragmentWhole()
 	part2.ApplyExpansions()
 	// fmt.Println(part2.Decode())
-	fmt.Println("Part2: ", part2.Checksum())
+	fmt.Println("Part2: ", part2.Checksum(), time.Since(start))
 }

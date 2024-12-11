11ea154379e2ae1ab7765cc9d05eacfb14c0921d
Adel Sehic <adelsehic95@gmail.com>
2024-12-11 21:18:38 +0100

    - Day11 part 1 completely changed, caching is wip

--- day11/main.go
+++ day11/main.go
@@ -4,28 +4,67 @@ import (
 	"fmt"
 	"os"
 	"time"
+	// "os"
 )
 
 const (
-	INPUT      = "input"
-	TEST1      = "test1"
-	TEST2      = "test2"
+	INPUT1 = "input1"
+	INPUT2 = "input2"
+	TEST1  = "test1"
+	TEST2  = "test2"
 )
 
-func (in *Input) Blink(times int) {
-	for i := 0; i < times; i++ {
-		evolution := make([]*Stone, 0)
-		for _, stone := range in.Stones {
-			evolution = append(evolution, stone.Evolve()...)
-		}
-		in.Stones = evolution
-		fmt.Println(i + 1)
+func RecursiveEvolve(number uint64, exit, iteration int) uint64 {
+	if iteration >= exit {
+		return 1
 	}
+	fmt.Println(iteration)
+
+	if number == 0 {
+		return RecursiveEvolve(1, exit, iteration+1)
+	}
+
+	digits := []uint64{}
+	modifiedNumber := number
+	for modifiedNumber > 0 {
+		digits = append([]uint64{modifiedNumber % 10}, digits...)
+		modifiedNumber /= 10
+	}
+
+	if len(digits)%2 == 0 {
+		mid := len(digits) / 2
+		left := uint64(0)
+		right := uint64(0)
+		for i := 0; i < mid; i++ {
+			left = left*10 + digits[i]
+			right = right*10 + digits[mid+i]
+		}
+		return RecursiveEvolve(left, exit, iteration+1) + RecursiveEvolve(right, exit, iteration+1)
+	}
+	return RecursiveEvolve(number*2024, exit, iteration+1)
 }
 
 func main() {
 	start := time.Now()
-	input := MakeInput(os.Args[1])
-	input.Blink(25)
-	fmt.Println("Part1: ", input.StoneCount(), time.Since(start))
+	sum := 0
+	input, exit := MakeInput(os.Args[1])
+	for _, v := range input {
+		sum += int(RecursiveEvolve(v, exit, 0))
+	}
+	fmt.Println(sum, time.Since(start))
+}
+
+func MakeInput(what string) ([]uint64, int) {
+	switch what {
+	case TEST1:
+		return []uint64{0, 1, 10, 99, 999}, 1
+	case TEST2:
+		return []uint64{125, 17}, 6
+	case INPUT1:
+		return []uint64{8435, 234, 928434, 14, 0, 7, 92446, 8992692}, 25
+	case INPUT2:
+		return []uint64{8435, 234, 928434, 14, 0, 7, 92446, 8992692}, 75
+	default:
+		return []uint64{0}, 10
+	}
 }

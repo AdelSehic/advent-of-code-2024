904a48ebe6d308ecfd6d4da6115d79d4f1546023
Adel Sehic <adelsehic95@gmail.com>
2024-12-12 20:46:46 +0100

    - Day12 part1 done

--- day12/main.go
+++ day12/main.go
@@ -7,6 +7,29 @@ import (
 	"github.com/AdelSehic/advent-of-code-2024/helpers"
 )
 
+type Plot struct {
+	Input  *helpers.Field
+	Places []*helpers.Coord
+	Fences uint64
+}
+
+func (p *Plot) PrintPlot() {
+	for _, k := range p.Places {
+		fmt.Printf("{%d, %d}", k.X, k.Y)
+	}
+	fmt.Printf(" - Fence size: %d\r\n", p.Fences)
+}
+
+func (p *Plot) CalculateFenceSize() {
+	for _, v := range p.Places {
+		p.Fences += CheckBorder(p.Input, v)
+	}
+}
+
+func (p *Plot) GetPrice() int {
+	return len(p.Places) * int(p.Fences)
+}
+
 func CheckBorder(input *helpers.Field, place *helpers.Coord) uint64 {
 	letter := input.GetLetter(place)
 	sum := uint64(0)
@@ -42,34 +65,34 @@ func FindPlot(letter byte, input *helpers.Field, place *helpers.Coord) []*helper
 func main() {
 	var input helpers.Field
 	input.LoadDataWithPadding(os.Args[1], "*")
-
 	letterPlaces := input.ValuePlaces('*')
 
-	plots := make(map[byte][][]*helpers.Coord)
+	plots := make(map[byte][]*Plot)
+	visited := make(map[helpers.Coord]bool)
 	for letter, places := range letterPlaces {
 		for _, coord := range places {
-			if input.GetLetter(coord) == '*' {
+			if visited[*coord] {
 				continue
 			}
-			plots[letter] = append(plots[letter], FindPlot(letter, &input, coord))
-		}
-	}
 
-	for k, v := range plots {
-		fmt.Printf("----------- %s -----------\r\n", string(k))
-		for _, i := range v {
-			for _, k := range i {
-				fmt.Printf("{%d, %d}", k.X, k.Y)
+			p := &Plot{
+				Input:  &input,
+				Places: FindPlot(letter, input.Copy(), coord),
 			}
-			fmt.Println()
+			p.CalculateFenceSize()
+			plots[letter] = append(plots[letter], p)
+
+			for _, c := range p.Places {
+				visited[*c] = true
+			}
 		}
-		fmt.Printf("-------------------------\r\n")
 	}
 
-	// part1 := uint64(0)
-	// for k, v := range fences {
-	// 	part1 += v * uint64(len(letterPlaces[k]))
-	// 	fmt.Printf("%s - %d\r\n", string(k), v)
-	// }
-	// fmt.Println("Part1 :", part1)
+	part1 := uint64(0)
+	for _, v := range plots {
+		for _, p := range v {
+			part1 += uint64(p.GetPrice())
+		}
+	}
+	fmt.Println("Part1 :", part1)
 }

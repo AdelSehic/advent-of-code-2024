13eba3b93ba02a329629bb05ea3126e7145fb3e1
Adel Sehic <adelsehic95@gmail.com>
2024-12-15 14:37:10 +0100

    - Day15 part2 - tests are passing but input is too low

--- day15/main.go
+++ day15/main.go
@@ -21,13 +21,38 @@ func Move(startPos *helpers.Coord, field *helpers.Field, direction byte) *helper
 	case helpers.LETTER_RIGHT:
 		directionFunc = (*helpers.Coord).Right
 	}
-	if moveHelper(startPos, field, directionFunc) {
+
+	if moveHelper(startPos, field, direction, directionFunc) {
 		return directionFunc(startPos)
 	}
 	return startPos
 }
 
-func moveHelper(position *helpers.Coord, field *helpers.Field, moveFunc func(*helpers.Coord) *helpers.Coord) bool {
+func dfWallSearch(position *helpers.Coord, field *helpers.Field, direction byte, moveFunc func(*helpers.Coord) *helpers.Coord) bool {
+	if position == nil {
+		return false
+	}
+	next := moveFunc(position)
+	letter := field.GetLetter(next)
+	if letter == '#' {
+		return true
+	}
+	if letter == '.' {
+		return false
+	}
+	hash := string(letter) + string(direction)
+	var halfBox *helpers.Coord
+	halfBox = nil
+	switch hash {
+	case "[^", "[v":
+		halfBox = next.Right()
+	case "]^", "]v":
+		halfBox = next.Left()
+	}
+	return dfWallSearch(halfBox, field, direction, moveFunc) || dfWallSearch(halfBox, field, direction, moveFunc)
+}
+
+func moveHelper(position *helpers.Coord, field *helpers.Field, direction byte, moveFunc func(*helpers.Coord) *helpers.Coord) bool {
 	next := moveFunc(position)
 	letter := field.GetLetter(next)
 	if letter == '#' {
@@ -36,10 +61,37 @@ func moveHelper(position *helpers.Coord, field *helpers.Field, moveFunc func(*he
 	if letter == '.' {
 		return true
 	}
-	if moveHelper(next, field, moveFunc) {
-		next = moveFunc(next)
-		field.SetLetter(next, letter)
-		return true
+
+	if dfWallSearch(position, field, direction, moveFunc) {
+		return false
+	}
+
+	hash := string(letter) + string(direction)
+	var halfBox *helpers.Coord
+	halfBox = nil
+	otherLetter := byte('.')
+	switch hash {
+	case "[^", "[v":
+		halfBox = next.Right()
+		otherLetter = ']'
+	case "]^", "]v":
+		halfBox = next.Left()
+		otherLetter = '['
+	}
+
+	if halfBox != nil {
+		if moveHelper(next, field, direction, moveFunc) && moveHelper(halfBox, field, direction, moveFunc) {
+			field.SetLetter(next, '.')
+			field.SetLetter(halfBox, '.')
+			field.SetLetter(moveFunc(next), letter)
+			field.SetLetter(moveFunc(halfBox), otherLetter)
+			return true
+		}
+	} else {
+		if moveHelper(next, field, direction, moveFunc) {
+			field.SetLetter(moveFunc(next), letter)
+			return true
+		}
 	}
 	return false
 }
@@ -47,7 +99,7 @@ func moveHelper(position *helpers.Coord, field *helpers.Field, moveFunc func(*he
 func main() {
 	input := &helpers.Field{}
 	input.LoadDataWithPadding(os.Args[1], "#")
-	input.PrintData()
+	part2 := CreatePart2(input)
 
 	robotPlace := input.FindLetter(input.MakeAllCoords(), '@')
 	robot := helpers.NewFieldIterator(robotPlace[0])
@@ -75,7 +127,50 @@ func main() {
 	part1 := 0
 	boxPlaces := input.FindLetter(input.MakeAllCoords(), 'O')
 	for _, v := range boxPlaces {
-		part1 += (v.Y-1) * 100 + v.X-1
+		part1 += (v.Y-1)*100 + v.X - 1
 	}
 	fmt.Println("Part1: ", part1)
+
+	robot2place := part2.FindLetter(part2.MakeAllCoords(), '@')
+	robot2 := helpers.NewFieldIterator(robot2place[0])
+	for _, dir := range directions {
+		part2.SetLetter(robot2.Position, '.')
+		robot2.Position = Move(robot2.Position, part2, byte(dir))
+		part2.SetLetter(robot2.Position, byte(dir))
+	}
+	part2.PrintData()
+
+	part2result := 0
+	boxPlacesPart2 := part2.FindLetter(part2.MakeAllCoords(), '[')
+	for _, v := range boxPlacesPart2 {
+		part2result += (v.Y-1)*100 + v.X - 2
+	}
+	fmt.Println("Part2: ", part2result)
+}
+
+func CreatePart2(input *helpers.Field) *helpers.Field {
+	field := &helpers.Field{
+		Width: 2 * input.Width,
+		Lines: make([]string, len(input.Lines)),
+	}
+	field.Width = 2 * input.Width
+	i := 0
+	for _, line := range input.Lines {
+		var sb strings.Builder
+		for _, char := range line {
+			switch char {
+			case '#':
+				sb.WriteString("##")
+			case '@':
+				sb.WriteString("@.")
+			case 'O':
+				sb.WriteString("[]")
+			default:
+				sb.WriteString("..")
+			}
+		}
+		field.Lines[i] = sb.String()
+		i++
+	}
+	return field
 }
